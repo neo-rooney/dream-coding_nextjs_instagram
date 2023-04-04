28530b453e313d2daf3fe895778ed3f447c173eb
rooney <bch3454@gmail.com>
2023-04-04 23:33:55 +0900

    Feat: 데이터베이스에 사용자 추가하기

--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -1,3 +1,4 @@
+import { addUser } from "@/service/user";
 import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 export const authOptions: NextAuthOptions = {
@@ -8,6 +9,17 @@ export const authOptions: NextAuthOptions = {
     }),
   ],
   callbacks: {
+    async signIn({ user: { id, name, image, email } }) {
+      if (!email) return false;
+      addUser({
+        id,
+        name: name || "",
+        image,
+        email,
+        username: email?.split("@")[0],
+      });
+      return true;
+    },
     async session({ session }) {
       const user = session?.user;
       if (user) {
